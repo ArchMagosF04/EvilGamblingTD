97a0b6d65fe80546c9ff7c2f6a6d0c5aa18eb3c2
ArchMagosF04 <francochialvo2004@gmail.com>
2026-09-06 20:20:41 -0300

    Wave Blueprint Fix

--- Assets/_Scripts/Enemies/EnemySpawning/SO_EnemyWavesBlueprint.cs
+++ Assets/_Scripts/Enemies/EnemySpawning/SO_EnemyWavesBlueprint.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using Alchemy.Serialization;
 using System;
 using System.Collections.Generic;
+using Alchemy.Inspector;
 
 [CreateAssetMenu(fileName = "New EnemyWavesBlueprint", menuName = "Scriptable Objects/Enemies/Enemy Waves Blueprint"), AlchemySerialize]
 public partial class SO_EnemyWavesBlueprint : ScriptableObject
 {
+    [HelpBox("When editing the dictionary change the value of this number so the changes are saved.")]
+    [SerializeField, Range(0f,1f)] private float change;
     [AlchemySerializeField, NonSerialized] public Dictionary<int, List<SpawnEntry>> EnemyWaves = new();
 }
